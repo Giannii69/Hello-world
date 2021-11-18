42ef189ac2f01ab4379efebea8aa6e6537744233
beau.gianni9@gmail.com <beau.gianni9@gmail.com>
2021-11-18 16:42:50 +0100

    init

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,65 @@
+<template>
+
+  <h1>hello</h1>
+
+  <ul id="example-1">
+
+    <li v-for="item in items" :key="item.message">
+
+      {{ item.message }}
+
+    </li>
+
+  </ul>
+
+</template>
+
+
+
+<script>
+
+export default {
+
+  name: 'HelloWorld',
+
+  props: {
+
+    msg: String
+
+  },
+
+  data(){
+
+    return {
+
+      items: [
+
+        {message:'Tom'},
+
+        {message:'Gianni'},
+
+        {message:'Laetitia'},
+
+        {message:'Sammy'},
+
+        {message:'George Gaming'}
+
+      ]
+
+    }
+
+  }
+
+}
+
+</script>
+
+
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+
+<style scoped>
+
+
+
+</style>
